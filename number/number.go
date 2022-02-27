3c6529904557624cd6bbf542c5a6d460851e6885
zhangxi <zhangxi.7702@bytedance.com>
2022-02-27 19:20:32 +0800

    feat: get max min value

--- number/number.go
+++ number/number.go
@@ -1,9 +1,10 @@
 package number
 
-func getPositiveNum(nums []int) []int{
-	res := make([]int,0, len(nums))
-	n := len(nums)-1
-	for i := 0; i<= n;i++ {
+// 得到组合中的所有正数
+func GetPositiveNum(nums []int) []int {
+	res := make([]int, 0, len(nums))
+	n := len(nums) - 1
+	for i := 0; i <= n; i++ {
 		if nums[i] > 0 {
 			res = append(res, nums[i])
 		}
@@ -11,12 +12,43 @@ func getPositiveNum(nums []int) []int{
 	return res
 }
 
-func getNegativeNum(nums []int) []int{
-	res := make([]int,0, len(nums))
-	n := len(nums)-1
-	for i := 0; i<= n;i++ {
+// 得到组合中的所有负数
+func GetNegativeNum(nums []int) []int {
+	res := make([]int, 0, len(nums))
+	n := len(nums) - 1
+	for i := 0; i <= n; i++ {
 		if nums[i] < 0 {
 			res = append(res, nums[i])
+		}
+	}
+	return res
+}
+
+func GetMinVaule(nums []int) int {
+	if len(nums) == 0 {
+		return 0
+	}
+
+	res := nums[0]
+	n := len(nums) - 1
+	for i := 1; i <= n; i++ {
+		if nums[i] < res {
+			res = nums[i]
+		}
+	}
+	return res
+}
+
+func GetMaxValue(nums []int) int {
+	if len(nums) == 0 {
+		return 0
+	}
+
+	res := nums[0]
+	n := len(nums) - 1
+	for i := 1; i <= n; i++ {
+		if nums[i] > res && nums[i] < 100000 {
+			res = nums[i]
 		}
 	}
 	return res
